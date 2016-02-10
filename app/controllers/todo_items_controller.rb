4f5372fd628e85a068b1a1b30b34876ada1f5fa4
Robert Ellefson <noetic33@gmail.com>
2016-02-09 20:48:33 -0800

    modified file to address bin/rake failure

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -10,7 +10,7 @@ class TodoItemsController < ApplicationController
 
   def create
   	@todo_list = TodoList.find(params[:todo_list_id])
-  	@todo_item = @todo_list.todo_items.new(todo_items_params)
+  	@todo_item = @todo_list.todo_items.new(todo_item_params)
  	if @todo_item.save
  		flash[:success] = "Added todo list item"
  		redirect_to todo_list_todo_items_path
